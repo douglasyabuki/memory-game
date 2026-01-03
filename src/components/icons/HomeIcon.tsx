4bef76e8f504aeafd747c4fc8a022cffd7b35e38
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2026-01-03 10:12:00 -0300

    Add: Icons

---
+++ src/components/icons/HomeIcon.tsx
@@ -0,0 +1,13 @@
+import { type Component } from "solid-js";
+
+export const HomeIcon: Component = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+  </svg>
+);
